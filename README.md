71a5eb194b4c3a984cb8ce132d913ba8674fc42b
hill <gaoshan@ihealthlabs.com.cn>
2024-07-22 18:29:37 +0800

    完成项目demo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cozeGin
 the project is woking for coze. with gin 
+
+## 文件结构
+
+cozeGin/
+├── conf
+├── middleware
+├── models
+├── pkg
+├── routers
+└── runtime
+
---
+++ routers/routers.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"coze_gin/pkg/setting"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	gin.SetMode(setting.RunMode)
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"user_name":      "张三",
+			"user_phone":     "13000001111",
+			"user_hosipital": "天津师范医院",
+			"user_team":      "李四主任团队",
+			"context":        "餐后血糖2.8已超出警戒值请及时干预",
+		})
+	})
+
+	r.GET("/get_user", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"user_name":      "张三",
+			"user_phone":     "13000001111",
+			"user_hosipital": "天津师范医院",
+			"user_team":      "李四主任团队",
+			"status":         "take_over",
+			"context":        "规范用户",
+		})
+	})
+
+	return r
+}
